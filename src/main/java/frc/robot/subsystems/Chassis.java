9f622aaa7494abcdb669701cad92c9762f54a287
richpants <rmansfield@creightonprep.org>
2021-07-04 10:53:09 -0500

    intial

---
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Chassis extends SubsystemBase {
+  /** Creates a new Chassis. */
+  public Chassis() {
+    leftMotor = new PWMVictorSPX(Constants.LEFT_MOTOR_PORT);
+    rightMotor = new PWMVictorSPX(Constants.RIGHT_MOTOR_PORT);
+    differentialDrive = new DifferentialDrive(leftMotor, rightMotor);
+    differentialDrive.setRightSideInverted(false);
+    differentialDrive.setSafetyEnabled(false);
+    leftEncoder = new Encoder(Constants.LEFT_ENCODER_A, Constants.LEFT_ENCODER_B);
+    rightEncoder = new Encoder(Constants.RIGHT_ENCODER_A, Constants.RIGHT_ENCODER_B);
+    leftEncoder.setDistancePerPulse(Constants.INCHES_PER_PULSE);
+    rightEncoder.setDistancePerPulse(Constants.INCHES_PER_PULSE);
+    addChild("leftEncoder", leftEncoder);
+    addChild("rightEncoder", rightEncoder);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
